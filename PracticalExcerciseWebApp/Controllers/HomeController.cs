cad8a71da9cb9553dfebc89a31f4ae369903f51a
thirupathi <athirupathi@delaplex.com>
2022-07-31 20:02:44 +0530

    final changes

--- PracticalExcerciseWebApp/Controllers/HomeController.cs
+++ PracticalExcerciseWebApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PracticalExcerciseWebApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,36 @@ namespace PracticalExcerciseWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<Appointment> lstApp;
+
         public ActionResult Index()
         {
+            string practitioners = Server.MapPath("~/App_Data/") + "practitioners.json";
+            string appointments = Server.MapPath("~/App_Data/") + "appointments.json";
+            List<Practitioner> lstPra = JsonConvert.DeserializeObject<List<Practitioner>>(System.IO.File.ReadAllText(practitioners));
+            lstApp = JsonConvert.DeserializeObject<List<Appointment>>(System.IO.File.ReadAllText(appointments));
+            ViewBag.Appointment = lstApp;
+            ViewBag.Practitioner = lstPra.Select(x => new SelectListItem { Text = x.name, Value = x.id.ToString() });
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetReport(string date,int pract)
+        {
+           DateTime _date= Convert.ToDateTime(date);
+            date = _date.ToString("M/d/yyyy").Replace('-', '/');
+            var data = lstApp.Where(x => x.date == date && x.practitioner_id == pract).GroupBy(x => new { x.practitioner_id, x.cost, x.revenue })
+                .Select(x => new { practitioner_id = x.Key.practitioner_id, cost = x.Key.cost, revenue = x.Key.revenue }).ToList();
+            return Json(data,JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GetDetailReport(string date, int pract)
+        {
+            DateTime _date = Convert.ToDateTime(date);
+            date = _date.ToString("M/d/yyyy").Replace('-', '/');
+            var data = lstApp.Where(x => x.date == date && x.practitioner_id == pract);
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult About()
---
+++ PracticalExcerciseWebApp/Models/Practitioner.cs
@@ -0,0 +1,20 @@
+﻿namespace PracticalExcerciseWebApp.Models
+{
+    public class Appointment
+    {
+        public int id { get; set; }
+        public string date { get; set; }
+        public string client_name { get; set; }
+        public string appointment_type { get; set; }
+        public int duration { get; set; }
+        public int revenue { get; set; }
+        public int cost { get; set; }
+        public int practitioner_id { get; set; }
+    }
+
+    public class Practitioner
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
